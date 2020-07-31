f73c42bb31e429e421f47561d34cfee432c22066
ernie0817 <ernie0817@gmail.com>
2020-07-31 17:20:14 +0800

    ini

--- app_core.py
+++ app_core.py
@@ -8,14 +8,7 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-import configparser
-
 # LINE 聊天機器人的基本資料
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-#line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-#handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 line_bot_api = LineBotApi('mPWcLzfZ80c9sPnTZe8sCrQxBuXhVvd8UCmrYhPKNn6+4P+CS8en7tG4u4lt0lCxT6zHPs+fDSuDzx0bSeuqvcW8fA885ktKefHkoSXw4etD8rzA73M2AXRTKUORo9c6ImLaO86kjYUxbqgKmk90FgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('ba597a8d56c7986d140690fb97151b8d')
 
@@ -37,10 +30,12 @@ def callback():
 # 學你說話
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
 if __name__ == "__main__":
     app.run()
