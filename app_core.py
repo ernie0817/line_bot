cbb94ba59bd679cadf043e8d138160816efd4a42
ernie0817 <ernie0817@gmail.com>
2020-08-03 17:12:08 +0800

    wo

--- app_core.py
+++ app_core.py
@@ -40,8 +40,8 @@ def pixabay_isch(event):
     line_bot_api.reply_message(
         event.reply_token,
         ImageSendMessage(
-            original_content_url='https://i.imgur.com/PPJOlv8.jpg',
-            preview_image_url='https://i.imgur.com/PPJOlv8.jpg'
+            original_content_url='https://i.imgur.com/JVXiiin.png',
+            preview_image_url='https://i.imgur.com/JVXiiin.png'
         )
     )
 
