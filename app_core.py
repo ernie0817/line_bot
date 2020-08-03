b10ee246926d17d7bf1a12e12bf149c44f9d865d
ernie0817 <ernie0817@gmail.com>
2020-08-03 11:28:48 +0800

    picture

--- app_core.py
+++ app_core.py
@@ -33,9 +33,10 @@ def pixabay_isch(event):
     
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
         try:
-            url = f"https://pixabay.com/images/search/{urllib.parse.urlencode({'q':event.message.text})[2:]}/"
+            q_string = {'tbm': 'isch', 'q': event.message.text}
+            url = f"https://www.google.com/search?{urllib.parse.urlencode(q_string)}/"
             headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
-            
+
             req = urllib.request.Request(url, headers = headers)
             conn = urllib.request.urlopen(req)
             
