e7c6cc990018b3d01c1a9236e1614321136d8ee7
ernie0817 <ernie0817@gmail.com>
2020-09-04 16:13:35 +0800

    20200904

--- custom_models/PhoebeTalks.py
+++ custom_models/PhoebeTalks.py
@@ -23,27 +23,27 @@ line_bot_api = LineBotApi('mPWcLzfZ80c9sPnTZe8sCrQxBuXhVvd8UCmrYhPKNn6+4P+CS8en7
 
 
 # 請 LINE 幫我們存入資料
-# def insert_record(event):
-#     if '草泥馬訓練紀錄' in event.message.text:
-#
-#         try:
-#             record_list = utils.prepare_record(event.message.text)
-#             reply = CallDatabase.line_insert_record(record_list)
-#
-#             line_bot_api.reply_message(
-#                 event.reply_token,
-#                 TextSendMessage(text=reply)
-#             )
-#
-#         except:
-#             line_bot_api.reply_message(
-#                 event.reply_token,
-#                 TextSendMessage(text='失敗了')
-#             )
-#
-#         return True
-#     else:
-#         return False
+def insert_record(event):
+    if '草泥馬訓練紀錄' in event.message.text:
+
+        try:
+            record_list = utils.prepare_record(event.message.text)
+            reply = CallDatabase.line_insert_record(record_list)
+
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply)
+            )
+
+        except:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='失敗了')
+            )
+
+        return True
+    else:
+        return False
 
 
 # 請 pixabay 幫我們找圖
