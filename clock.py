1e11f3416ca2ba5a75511e40309cedfee0854170
ernie0817 <ernie0817@gmail.com>
2020-09-11 17:06:19 +0800

    20200911

--- clock.py
+++ clock.py
@@ -31,7 +31,7 @@ def scheduled_job():
         print(key, value)
 
 
-@sched.scheduled_job('cron', day_of_week='fri', hour=17, minute=0)
+@sched.scheduled_job('cron', day_of_week='fri', hour=17, minute=10)
 def scheduled_job():
     print('========== APScheduler CRON =========')
     print('This job is run every weekday at 6:30')
