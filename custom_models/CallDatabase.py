59d84c16c003f4f7b551b377af5a36a541c62f3b
ernie0817 <ernie0817@gmail.com>
2020-09-15 14:57:45 +0800

    20200915

--- custom_models/CallDatabase.py
+++ custom_models/CallDatabase.py
@@ -13,7 +13,7 @@ def line_insert_record(record_list):
     table_columns = '(user_id, user_name, participate, date)'
     postgres_insert_query = f"""INSERT INTO order_meal {table_columns} VALUES (%s, %s, %s, %s)"""
     #
-    cursor.executemany(postgres_insert_query, record_list)
+    cursor.execute(postgres_insert_query, record_list)
     conn.commit()
 
     message = '資料匯入成功!'
