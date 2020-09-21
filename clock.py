c93b591ea8025d770667357a992c9e4dc539911d
ernie0817 <ernie0817@gmail.com>
2020-09-21 16:23:59 +0800

    20200921

--- clock.py
+++ clock.py
@@ -33,7 +33,7 @@ sched = BlockingScheduler()
 #     line_bot_api.push_message("Ubef4ab85bdc358ebca5ef6969763f5b6", TextSendMessage(text="記得訂便當喔!"))
 
 
-@sched.scheduled_job('cron', day_of_week=0, hour=16, minute=23)
+@sched.scheduled_job('cron', day_of_week=1, hour=10, minute=10)
 def scheduled_job():
     print('========== APScheduler CRON =========')
     print('This job is run every weekday at 6:30')
